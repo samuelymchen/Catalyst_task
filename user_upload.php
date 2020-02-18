425388fa63d2835f487b8b5d3458c9379c1ea611
Samuel Yi Ming Chen <sche0005@student.monash.edu>
2020-02-18 18:16:48 +1100

    User must provide -u, -p, -h to connect to db

--- user_upload.php
+++ user_upload.php
@@ -2,9 +2,9 @@
 // We assume the csv file is called users.csv, unless being specified
 $file_name = 'users.csv';
 $dry_run = false;
+$create_mode = false;
 
 if ($argc > 1) {
-    $conn = connect_db('samuel', 'testtest', '127.0.0.1');
     // Start from the second index
     for ($i = 1; $i < $argc; $i++) {
         // Skip current iteration if it is a file name which is surrounded by []
@@ -27,17 +27,69 @@ if ($argc > 1) {
                 die($message);
             }
         } else if ($argv[$i] === '--create_table') {
-            create_table($conn);
-            exit();
+            $create_mode = true;
         } else if ($argv[$i] === '--dry_run') {
             $dry_run = true;
-            echo 'dry run';
         } else if ($argv[$i] === '--help') {
             echo 'help';
         } else {
             // Check if -u, -p, -h is supplied
+            $get_parameter = substr($argv[$i], 0, 2);
+            if ($get_parameter === '-u' || $get_parameter === '-p' || $get_parameter === '-h') {
+                $parameter_val = substr($argv[$i], 3);
+
+                if ($parameter_val !== '' && strlen($parameter_val) !== 0) {
+                    switch ($get_parameter) {
+                        case '-u':
+                            $user = $parameter_val;
+                            break;
+                        case '-p':
+                            $password = $parameter_val;
+                            break;
+                        case '-h':
+                            $host = $parameter_val;
+                            break;
+                    }
+                } else {
+                    $message = "Error: Database user, password or host are not supplied correctly, please run $ php user_upload.php --help for more information\n";
+                    die($message);
+                }
+
+            } else {
+                // If other parameters are given, exit and show the help page
+                $message = "Error: '".$argv[$i]."' is not a valid parameter, please run $ php user_upload.php --help for more information\n";
+                die($message);
+            }
         }
     }
+
+    if (!isset($user)) {
+        $message = "Error: User is not supplied\n";
+        die($message);
+    }
+
+    if (!isset($password)) {
+        $message = "Error: Password is not supplied\n";
+        die($message);
+    }
+
+    if (!isset($host)) {
+        $message = "Error: Host is not supplied\n";
+        die($message);
+    }
+
+        $conn = connect_db($user, $password, $host);
+
+//    $conn = connect_db('samuel', 'testtest', '127.0.0.1');
+
+    // If --create_table is presented, create table only and no more action
+    if ($create_mode) {
+        create_table($conn);
+        exit();
+    }
+
+    // If nothing goes wrong, read csv file and store
+
 } else {
     $message = "Error: No parameter is presented, please run $ php user_upload.php --help for more information\n";
     die($message);
